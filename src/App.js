64781a6b257a98f8548a07e0728cde9b62d0046f
Chen <97513965+OrangeKami@users.noreply.github.com>
2023-04-02 17:45:38 +1000

    layout
    layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
 import SideLinks from "./components/SideLinks/SideLinks.jsx";
 import SideMail from "./components/SideEmail/SideMail.jsx";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
-      <SideLinks />
-      <SideMail />
+      <div className="main">
+        <SideLinks />
+        <SideMail />
+      </div>
       <Footer />
     </>
   );
